3f10ab51be88746fb701121a8190c064ec364ba8
Karthik <karthik.krishnaswamy17@gmail.com>
2021-10-12 16:49:26 +0530

    Updates

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,7 @@
+#!/usr/bin/env groovy
+@Library('shared-jenkin-lib')
 def gv
+
 pipeline{
 agent any
 tools{
@@ -24,7 +27,7 @@ stages{
             
             script{
                 
-                gv.buildJar()
+                buildJar()
             }
         }
     }
@@ -33,7 +36,7 @@ stages{
     stage("build image"){
         steps{
             script{
-                gv.buildImage()
+                buildImage()
             }
             
         }
--- script.groovy
+++ script.groovy
@@ -1,20 +1,4 @@
-#!/bin/bash/env groovy
-def buildJar(){
-    echo "Building Jar...."
-    sh 'mvn package'
-
-}
-
-def buildImage(){
-        script{
-                echo "Building Image"
-                withCredentials([usernamePassword(credentialsId:'docker-hub-account',usernameVariable:'user',passwordVariable:'pass')]){
-                sh 'docker build -t karthik0517/java-maven-app:${BUILD_NUMBER} .'
-                sh "echo $pass |docker login --username $user --password-stdin"
-                sh 'docker push karthik0517/java-maven-app:${BUILD_NUMBER}'
-                }
-              }
-}
+#!/usr/bin/env groovy
 
 def deployApp(){
     echo "Deploying...groovy"
